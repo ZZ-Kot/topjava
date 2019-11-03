973cfef1c1704ccb6b14b9f0b12e13ff5c51bf46
Konstantin K <dev.konstantin.k@gmail.com>
2019-11-03 09:09:17 +0200

    Проект пропатчен перед HW05.

---
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity_.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-11-02T08:49:01.754+0200")
+@StaticMetamodel(AbstractBaseEntity.class)
+public class AbstractBaseEntity_ {
+	public static volatile SingularAttribute<AbstractBaseEntity, Integer> id;
+}
---
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity_.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-11-02T08:49:01.854+0200")
+@StaticMetamodel(AbstractNamedEntity.class)
+public class AbstractNamedEntity_ extends AbstractBaseEntity_ {
+	public static volatile SingularAttribute<AbstractNamedEntity, String> name;
+}
