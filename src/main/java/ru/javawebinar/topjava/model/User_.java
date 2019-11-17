bee1fb980b8a629856e88294abb4712c094a2bc4
Konstantin K <dev.konstantin.k@gmail.com>
2019-11-17 10:34:27 +0200

    Мелкие правки.

--- src/main/java/ru/javawebinar/topjava/model/User_.java
+++ src/main/java/ru/javawebinar/topjava/model/User_.java
@@ -2,11 +2,12 @@ package ru.javawebinar.topjava.model;
 
 import java.util.Date;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-11-02T08:49:01.899+0200")
+@Generated(value="Dali", date="2019-11-17T10:29:46.263+0200")
 @StaticMetamodel(User.class)
 public class User_ extends AbstractNamedEntity_ {
 	public static volatile SingularAttribute<User, String> email;
@@ -15,4 +16,5 @@ public class User_ extends AbstractNamedEntity_ {
 	public static volatile SingularAttribute<User, Date> registered;
 	public static volatile SetAttribute<User, Role> roles;
 	public static volatile SingularAttribute<User, Integer> caloriesPerDay;
+	public static volatile ListAttribute<User, Meal> meals;
 }
